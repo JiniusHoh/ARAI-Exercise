f4eeae8fbfbd2069854b9bf680b6ef50965bb87f
JiniusHoh <95525678+JiniusHoh@users.noreply.github.com>
2024-06-06 11:24:35 +0800

    Update bicep.py

--- bicep.py
+++ bicep.py
@@ -205,8 +205,8 @@ import streamlit as st
 from webcam import webcam
 
 captured_image = webcam()
-if captured_image is None:
-    st.write("Waiting for capture...")
-else:
-    st.write("Got an image from the webcam:")
-    st.image(captured_image)
+# if captured_image is None:
+#     st.write("Waiting for capture...")
+# else:
+#     st.write("Got an image from the webcam:")
+#     st.image(captured_image)
