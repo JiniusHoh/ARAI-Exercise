77cc3e996dfdb9d1bc1a4a200ba1cfe319575c09
JiniusHoh <95525678+JiniusHoh@users.noreply.github.com>
2024-06-15 12:53:48 +0800

    Update bicep.py

--- bicep.py
+++ bicep.py
@@ -157,7 +157,7 @@ def app():
     key="full-body-detection",
     video_processor_factory=VideoProcessor,
     rtc_configuration=RTCConfiguration(
-        {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+        {"iceServers": [{"urls": ["stun:stun.xten.com:3478"]}]}
     ),
     media_stream_constraints={"video": {"frameRate": {"ideal": 15}}, "audio": False},
     video_html_attrs={
