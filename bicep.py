eb2f080f253776286088d77f8176dc9080890ba1
JiniusHoh <95525678+JiniusHoh@users.noreply.github.com>
2024-06-15 13:47:06 +0800

    Update bicep.py

--- bicep.py
+++ bicep.py
@@ -153,11 +153,50 @@ def app():
     st.image('bicep_curl_angle.png')
     st.markdown('**Perfect angle for a bicep curl is 45 degree to 60 degree. Try now with your left arm! Make sure to show your upper body with your left arm into your webcam.**')
 
+#     webrtc_ctx = webrtc_streamer(
+#     key="full-body-detection",
+#     video_processor_factory=VideoProcessor,
+#     rtc_configuration=RTCConfiguration(
+#         {"iceServers": [{"urls": ["stun:stun.xten.com:3478"]}]}
+#     ),
+#     media_stream_constraints={"video": {"frameRate": {"ideal": 15}}, "audio": False},
+#     video_html_attrs={
+#         "style": {"width": "50%", "margin": "0 auto", "border": "5px purple solid"},
+#         "controls": False,
+#         "autoPlay": True,
+#     },
+#     async_processing=True,
+# )
+
     webrtc_ctx = webrtc_streamer(
     key="full-body-detection",
     video_processor_factory=VideoProcessor,
     rtc_configuration=RTCConfiguration(
-        {"iceServers": [{"urls": ["stun:stun.xten.com:3478"]}]}
+        {iceServers: [
+      {
+        urls: "stun:stun.relay.metered.ca:80",
+      },
+      {
+        urls: "turn:global.relay.metered.ca:80",
+        username: "47d918d6e9c419663f5561b8",
+        credential: "6QcHngj15KNzDCrd",
+      },
+      {
+        urls: "turn:global.relay.metered.ca:80?transport=tcp",
+        username: "47d918d6e9c419663f5561b8",
+        credential: "6QcHngj15KNzDCrd",
+      },
+      {
+        urls: "turn:global.relay.metered.ca:443",
+        username: "47d918d6e9c419663f5561b8",
+        credential: "6QcHngj15KNzDCrd",
+      },
+      {
+        urls: "turns:global.relay.metered.ca:443?transport=tcp",
+        username: "47d918d6e9c419663f5561b8",
+        credential: "6QcHngj15KNzDCrd",
+      },
+  ]
     ),
     media_stream_constraints={"video": {"frameRate": {"ideal": 15}}, "audio": False},
     video_html_attrs={
@@ -167,7 +206,6 @@ def app():
     },
     async_processing=True,
 )
-
     video_processor = VideoProcessor()
 
     last_feedback = ""
